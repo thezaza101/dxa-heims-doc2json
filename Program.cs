1ee8d4d9212f9d4dbe9ddd421f7a0737657d1317
thezaza101 <thezaza102@gmail.com>
2018-03-14 15:09:24 +1100

    initial draft

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Linq;
+using System.Collections;
+
+namespace HEIMS_DOC_TO_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "";
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load("Data1.htm");
+            data = doc.GetElementbyId("WordSection3").InnerHtml;
+            System.Console.WriteLine(data);
+            Console.ReadLine();
+            
+        }
+    }
+}
